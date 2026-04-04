233d78438fe64018ddefb86f60f74aabbc1a7691
andrewtritchie <atr5366@psu.edu>
2026-04-04 18:18:06 -0400

    Works for determing the string validity using backwards reading, but only works assuming the first transition from each state is correct

--- nfa_q_accept.py
+++ nfa_q_accept.py
@@ -1,11 +1,13 @@
+test_string = "1"
+out = False
 with open("nfa.txt", 'r') as nfa:
     #get number of states
     num_states = int(nfa.readline().strip())
-    print("There are " + str(num_states) + " states")
+    #print("There are " + str(num_states) + " states")
 
     #get start state
     start_state = int(nfa.readline().strip())
-    print("q" + str(start_state) + " is the start state")
+    #print("q" + str(start_state) + " is the start state")
 
     #get number of accept states
     num_accepts = int(nfa.readline().strip())
@@ -14,11 +16,11 @@ with open("nfa.txt", 'r') as nfa:
     list_of_accepts = []
     for i in range(num_accepts):
         list_of_accepts.append(int(nfa.readline().strip()))
-    print("accept states:", list_of_accepts)
+    #print("accept states:", list_of_accepts)
 
     #get alphabet
     alphabet = list(nfa.readline().strip())
-    print("This is the alphabet: ", alphabet)
+    #print("This is the alphabet: ", alphabet)
 
     #get number of transitions
     num_transitions = int(nfa.readline().strip())
@@ -27,6 +29,28 @@ with open("nfa.txt", 'r') as nfa:
     trans = []
     for i in range(num_transitions):
         trans.append(nfa.readline().strip())
-    print("All transitions: ",trans)
+    #print("All transitions: ",trans)
 
 
+    for a in list_of_accepts:
+        current_state = a
+        dummy_string = test_string
+        
+        while(len(dummy_string)>0):
+            dummy_len = len(dummy_string)
+            for t in trans:
+                src, sym, dst = [x.strip() for x in t.split(',')]
+                if (current_state == int(dst) and (dummy_string[-1] == sym or sym == "E")):
+                    current_state = int(src)
+                    if sym != "E":
+                        dummy_string = dummy_string[:-1]
+                    break
+            #length did not change nor did state, no transitions worked
+            if dummy_len == len(dummy_string):
+                break
+        #If the dummy string is empty and we are at a start state, we return true
+        if len(dummy_string) == 0 and int(current_state) == start_state:
+            out = True
+    print(out)
+
+        
